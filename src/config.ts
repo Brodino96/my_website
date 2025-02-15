a2124f449bf8963b852a7318e5934b3e6c1b7964
Brodino <brodino96@gmail.com>
2025-02-15 19:29:10 +0100

    Made some refinement

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Vincent Acanfora"
-export const SITE_DESCRIPTION = ""
+export const SITE_DESCRIPTION = "My Website"
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
